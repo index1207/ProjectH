59eab19809fe6db8c805e24fe64f7dd7b37e2643
yj0216 <yeojun0216@naver.com>
2022-11-02 21:09:26 +0900

    ProjectStart

--- src/main/java/projecth/project_h/HelloController.java
+++ src/main/java/projecth/project_h/HelloController.java
@@ -5,10 +5,12 @@ import javafx.scene.control.Label;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label start;
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        start.setText("테스트");
     }
+
+
 }
