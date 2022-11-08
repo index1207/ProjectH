25d614d1cfd0fb5c93074e389426d89d29428242
yj0216 <yeojun0216@naver.com>
2022-11-08 21:17:13 +0900

    Project in the pipeline3

--- src/main/java/projecth/project_h/Login.java
+++ src/main/java/projecth/project_h/Login.java
@@ -15,14 +15,16 @@ import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
+import java.time.temporal.TemporalQueries;
 
 
 public class Login extends Application  {
 
     private StackPane root = new StackPane();
-    private Scene scene1,scene2,scene3,scene4;
+    private Scene scene1,scene2,scene3,scene4,scene5;
     private Stage window;
 
+    String UserId,pw,UserId2,pw2;
 
 
     @Override
@@ -30,61 +32,79 @@ public class Login extends Application  {
         window = primaryStage;
 
         //로그인 페이지 버튼
-        Label label1 = new Label("Username");
-        Label label2 = new Label("Password");
-        Button button1 = new Button("로그인");
-        Button button2 = new Button("회원가입");
+        Label Username = new Label("Username");
+        TextField user = new TextField();
+        Label Password = new Label("Password");
+        PasswordField password = new PasswordField();
+        Button login = new Button("로그인");
+        Button signUp = new Button("회원가입");
 
         //메인메뉴 페이지 버튼
-        Button button3 = new Button("시작하기");
-        Button button4 = new Button("게임방법");
-        Button button5 = new Button("나가기");
+        Button start = new Button("시작하기");
+        Button rule = new Button("게임방법");
+        Button exit = new Button("나가기");
 
         //게임방법 페이지 버튼
-        Label label3 = new Label("이 게임은 채팅을 칠 수 있는 메타버스 게임입니다.\n" +
+        Label rule2 = new Label("이 게임은 채팅을 칠 수 있는 메타버스 게임입니다.\n" +
                                     "~이러한 기능 들을 사용할 수 있으며\n" +
                                     "~게 이용 가능 합니다."
         );
-        Button button6 = new Button("돌아가기");
+        Button Return = new Button("돌아가기");
 
         //게임 페이지 버튼
 
 
-        //button1 눌렀을때(Login) -> 메인메뉴로
-        button1.setOnAction(e->window.setScene(scene2));
+        //회원가입 페이지 버튼
+        Label Username2 = new Label("Username");
+        TextField user2 = new TextField("Username");
+        Label Password2 = new Label("Password");
+        PasswordField password2 = new PasswordField();
+        Button register = new Button("등록");
 
-        //button2
+        //login 눌렀을때(Login) -> 메인메뉴로
+        login.setOnAction(e->window.setScene(scene2));
 
-        //button3 시작하기 -> ?
-        button3.setOnAction(e->window.setScene(scene1));
+        //signUp 회원가입 -> 회원가입 페이지
+        signUp.setOnAction(e->window.setScene(scene5));
 
-        //button4 게임방법 -> 게임방법 페이지
-        button4.setOnAction(e->window.setScene(scene3));
-        button6.setOnAction(e->window.setScene(scene2));//돌아가기 -> 메인메뉴
+        //start 시작하기 -> ?
+        start.setOnAction(e->window.setScene(scene1));
 
-        //button5 나가기 -> 시스템 종료
-        button5.setOnAction(e->window.close());
+        //rule 게임방법 -> 게임방법 페이지
+        rule.setOnAction(e->window.setScene(scene3));
+        Return.setOnAction(e->window.setScene(scene2));//돌아가기 -> 메인메뉴
+
+        //exit 나가기 -> 시스템 종료
+        exit.setOnAction(e->window.close());
+
+        //register 회원가입 -> 로그인
+        register.setOnAction(e->window.setScene(scene1));
 
         // layout1 = 로그인 페이지
         VBox layout1 = new VBox(10);
         layout1.setSpacing(8);
         layout1.setPadding(new Insets(10,10,10,10));
         layout1.getChildren().addAll(
-                label1,
-                new TextField(),
-                label2,
-                new PasswordField(),
-                button1,
-                button2);
+                Username,
+                user,
+                Password,
+                password,
+                login,
+                signUp
+        );
+        user.setPromptText("Username");
+        password.setPromptText("Password");
+        UserId = user2.getText();
+        pw = password2.getText();
         scene1 = new Scene(layout1, 400, 200);
 
         // layout2 = 메인메뉴 페이지
         VBox layout2 = new VBox(10);
-        layout2.setPadding(new Insets(90,10,10,10));
+        layout2.setPadding(new Insets(90,10,10,30));
         layout2.getChildren().addAll(
-                button3,
-                button4,
-                button5
+                start,
+                rule,
+                exit
         );
         scene2 = new Scene(layout2, 500,300);
 
@@ -109,8 +129,8 @@ public class Login extends Application  {
         layout3.setSpacing(8);
         layout3.setPadding(new Insets(50,10,10,10));
         layout3.getChildren().addAll(
-                label3,
-                button6
+                rule2,
+                Return
         );
         scene4 = new Scene(layout3, 400, 200);
 
@@ -120,10 +140,35 @@ public class Login extends Application  {
         layout4.setSpacing(8);
         layout4.setPadding(new Insets(50,10,10,10));
         layout4.getChildren().addAll(
-                label3,
-                button6
+                rule2,
+                Return
         );
         scene3 = new Scene(layout4, 400, 200);
+
+        //layout5 = 회원가입 페이지
+        VBox layout5 = new VBox(10);
+        layout5.setSpacing(8);
+        layout5.setPadding(new Insets(10,10,10,10));
+        layout5.getChildren().addAll(
+                Username2,
+                user2,
+                Password2,
+                password2,
+                register
+        );
+        user2.setPromptText("Username");
+        password2.setPromptText("Password");
+        UserId2 = user2.getText();
+        pw2 = password2.getText();
+        scene5 = new Scene(layout5, 400, 200);
+
+        //layout6 = 시작페이지
+        VBox layout6 = new VBox(10);
+        layout6.setSpacing(8);
+        layout6.setPadding(new Insets(50,10,10,10));
+        layout6.getChildren().addAll(
+
+        );
 
         //시작
         window.setScene(scene1);
